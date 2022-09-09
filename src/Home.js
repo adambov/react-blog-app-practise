759e8d90e3c558cc6e3e0805efce2e0550ae26e7
adambov <atanas.dambov@gmail.com>
2022-09-09 14:58:42 +0300

    second

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,17 @@
+import { useState } from "react";
+
 const Home = () => {
+   
 
-const handleClick = (e) => {
-    console.log('hello ninjas', e);
-}
-
-const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target);
-
+const handleClick = () => {
+   
 }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('Nasko', e)}>Click me again</button>
 
         </div>
     );
