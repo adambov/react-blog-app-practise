4ae0dc7d7039b8fdf2d9e912f3bb9bb9f0a1358d
adambov <atanas.dambov@gmail.com>
2022-09-09 17:56:15 +0300

    adding title

--- src/Home.js
+++ src/Home.js
@@ -10,8 +10,7 @@ const Home = () => {
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <BlogList blogs={blogs}/>
+            <BlogList blogs={blogs} title="All Blogs" />
         </div>
     );
 }
