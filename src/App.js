cb6d1ab0c0a93075d4b85a88b6f1bfe713779d49
adambov <atanas.dambov@gmail.com>
2022-09-09 12:26:05 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="content">
+        <Navbar></Navbar>
+        <Home></Home>
+      </div>
+    </div>
+  );
+}
+
+export default App;
